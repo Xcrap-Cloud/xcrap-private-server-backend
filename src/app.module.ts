d61ae39e8218d683a0c3e99b32706e2610449343
Marcuth <thiago.playgames15@gmail.com>
2025-07-08 01:42:43 -0300

    importando módulo de autenticação no módulo do app

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { ConfigModule } from "@nestjs/config"
 import { Module } from "@nestjs/common"
 
 import { UsersModule } from "./users/users.module"
+import { AuthModule } from "./auth/auth.module"
 
 @Module({
     imports: [
         ConfigModule.forRoot({ isGlobal: true }),
+        AuthModule,
         UsersModule,
     ],
 })
