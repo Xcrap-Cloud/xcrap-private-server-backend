bf3eea1c43ec41be83b6e13814bb13f5d621150c
Marcuth <thiago.playgames15@gmail.com>
2025-07-08 23:38:43 -0300

    Update users.module.ts

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,9 @@
 import { Module } from "@nestjs/common"
 
 import { PrismaModule } from "../prisma/prisma.module"
+import { CryptoModule } from "../crypto/crypto.module"
 import { UsersController } from "./users.controller"
 import { UsersService } from "./users.service"
-import { CryptoModule } from "../crypto/crypto.module"
 
 @Module({
     imports: [PrismaModule, CryptoModule],
